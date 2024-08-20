058d6bd9799849ce549c058feed52fcd8277f955
Stephen Horne <code@stephenhorne.com>
2024-08-20 12:54:13 -0400

    Add ArrayNode class
    * Add a polymorphic vector that can store the derived types
    * Add ::insert() method to take unique_ptrs to ValueNodes and steal them
    * Add tests covering getType(), getValue(), and Value Node store/fetch works as expected

---
+++ src/class/json/ArrayNode.hpp
@@ -0,0 +1,29 @@
+#ifndef ARRAY_NODE_H
+#define ARRAY_NODE_H
+
+#include "ValueNodeBase.hpp"
+
+namespace JSON {
+    class ArrayNode : public ValueNodeBase {
+        const Type type;
+        using StorageType = std::vector<std::unique_ptr<ValueNodeBase>>;
+        const std::unique_ptr<StorageType> value;
+
+    public:
+        ArrayNode() : type(Type::Array), value(std::make_unique<StorageType>()) {}
+
+        Type getType() const override {
+            return type;
+        }
+
+        void* getValue() const override {
+            return value.get();
+        }
+
+        void insert(std::unique_ptr<ValueNodeBase>&& up) {
+            value->push_back(std::move(up));
+        }
+    };
+}
+
+#endif // ARRAY_NODE_H
