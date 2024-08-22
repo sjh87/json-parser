6b6b6497b3c9e6077e290fcf5081f5069964dfb2
Stephen Horne <code@stephenhorne.com>
2024-08-22 15:31:30 -0400

    Introduce JSON::Parser class

---
+++ test/src/class/json/parser/Parser.test.cpp
@@ -0,0 +1,10 @@
+#include "Parser.test.hpp"
+
+namespace ParserTests {
+    Test::TestGroup tests{};
+
+    void init() {
+        tests.add({ "JSON::Parser class tests" });
+
+    }
+}
---
+++ test/src/class/json/parser/Parser.test.hpp
@@ -0,0 +1,11 @@
+#ifndef PARSER_TESTS_H
+#define PARSER_TESTS_H
+
+#include "../../../../src/class/test/TestRunner.hpp"
+
+namespace ParserTests {
+    extern Test::TestGroup tests;
+    void init();
+}
+
+#endif // PARSER_TESTS_H
